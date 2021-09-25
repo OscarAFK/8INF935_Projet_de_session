f697eb4cc1acbbd6c75f09a4e427b8803b153587
IQUELFergal <fergal.iquel@gmail.com>
2021-09-25 09:21:59 -0400

    Add ImGui to project

--- MPJV - Projet/MPJV - Projet/MPJV - Projet.cpp
+++ MPJV - Projet/MPJV - Projet/MPJV - Projet.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <iostream>
-#include "Particle.h"
-#include "Physics.h"
-#include <GLFW/glfw3.h>
-
-#include "Display.h"
 #include <chrono>
 #include <thread>
+
+#include "Particle.h"
+#include "Physics.h"
+#include "Display.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GLFW/glfw3.h>
+
+
 
 int main()
 {   
@@ -51,6 +58,23 @@ int main()
         break;
     }
   Display display = Display(&physic);
+
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  //ImGui::StyleColorsClassic();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#verion 330");
+
 	
   while (!glfwWindowShouldClose(window)) {
 		//Setup View
