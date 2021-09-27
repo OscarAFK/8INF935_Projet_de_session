b93119e5f46ac09b43d94563a9fa477d6e19f1d3
IQUELFergal <fergal.iquel@gmail.com>
2021-09-27 19:07:46 -0400

    Remove unused code

--- MPJV - Projet/MPJV - Projet/MPJV - Projet.cpp
+++ MPJV - Projet/MPJV - Projet/MPJV - Projet.cpp
@@ -45,11 +45,6 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 
 int main()
 {   
-	using namespace std::this_thread; // sleep_for, sleep_until
-	using namespace std::chrono; // nanoseconds, system_clock, seconds
-
-	float deltaTime = 0.01f;
-	
 	//GLFW initialization
 	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
@@ -70,46 +65,21 @@ int main()
 
 	//Initializing physics and display
 	Physics physic = Physics();
-	/*std::cout << "Bienvenue au stand de tir\nVeuillez choisir votre projectile.\n1 : Boulet de canon\n2 : Boule de feu\n3 : Laser\n4 : Balle\n5 : Projectile modifiable\nLes vecteurs sont au format(x,y,z)" << std::endl;
-	int choice;
-	std::cin >> choice;
-	switch (choice) {
-	case 1: std::cout << "vous avez choisi le boulet de canon" << std::endl;
-		physic.addParticle(0.01, 1, Vector3D(0, 0, 0), Vector3D(1, -5, 3), Vector3D(0, 0, 0));
-		break;
-	case 2: std::cout << "vous avez choisi la boule de feu" << std::endl;
-		physic.addParticle(0.05, 1, Vector3D(0, 0, 0), Vector3D(1, 2, 1), Vector3D(0, 0, 0));
-		break;
-	case 3: std::cout << "vous avez choisi le laser" << std::endl;
-		physic.addParticle(10000, 1, Vector3D(0, 0, 0), Vector3D(500, 0, 0), Vector3D(0, 0, 0));
-		break;
-	case 4: std::cout << "vous avez choisi la balle" << std::endl;
-		physic.addParticle(0.99, 1, Vector3D(0, 0, 0), Vector3D(10, 10, 0), Vector3D(0, 0, 0));
-		break;
-	case 5: std::cout << "vous avez choisi le projectile personnel" << std::endl;
-		break;
-	default:
-		return;
-
-	}*/
 	Display display = Display(&physic, WINDOW_SIZE_X, WINDOW_SIZE_Y);
 
 	#pragma region ImGui Setup
-  // Setup Dear ImGui context
-  IMGUI_CHECKVERSION();
-  ImGui::CreateContext();
-  ImGuiIO& io = ImGui::GetIO(); (void)io;
-  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-  //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-  // Setup Dear ImGui style
-  ImGui::StyleColorsDark();
-  //ImGui::StyleColorsClassic();
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
 
-  // Setup Platform/Renderer backends
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init("#version 330");
-#pragma endregion
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
+	#pragma endregion
 
 	float positions[6] = {
 		-0.5f, -0.5f,
@@ -117,15 +87,6 @@ int main()
 		0.5f, -0.5f
 	};
 
-	//Vertex buffer
-	/*unsigned int buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);*/
-	
 	std::string vertexShader =
 		"#version 330\n"\
 		"layout(location=0) in vec4 position;\n"\
