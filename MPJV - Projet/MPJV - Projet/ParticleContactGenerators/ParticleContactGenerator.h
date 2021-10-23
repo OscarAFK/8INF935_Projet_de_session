4c31de45077576c83768fb915983231cadfaad6b
Oscar Trosseau <57917670+OscarAFK@users.noreply.github.com>
2021-10-23 16:00:21 -0400

    Revert "Contacts"

--- MPJV - Projet/MPJV - Projet/ParticleContactGenerators/ParticleContactGenerator.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ParticleContact.h"
-#include <vector>
-
-class ParticleContactGenerator
-{
-public:
-	virtual unsigned int addContact(std::vector<ParticleContact*>* contact, unsigned int  limit) = 0;
-};
