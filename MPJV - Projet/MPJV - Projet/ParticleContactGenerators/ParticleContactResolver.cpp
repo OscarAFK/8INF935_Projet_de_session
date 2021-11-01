2477bebd2114082316ba0450281c4b9dfd2e1788
Osar <o.trosseau@laposte.net>
2021-11-01 13:51:48 -0400

    Fixed Particle contact resolver

--- MPJV - Projet/MPJV - Projet/ParticleContactGenerators/ParticleContactResolver.cpp
+++ MPJV - Projet/MPJV - Projet/ParticleContactGenerators/ParticleContactResolver.cpp
@@ -6,27 +6,23 @@ void ParticleContactResolver::resolveContacts(ParticleContact* contactArray, uns
 
 	//Cette fonction est à retravailler
 
-	/*if (numContact == 0) return;
+	if (numContact == 0) return;
 	m_iteration = 0;
 	float smallestRelativeVelocity = 1000;
 	do {
 		ParticleContact *contactToResolve = nullptr;
-		for (int i = 0; i < contactArray.size(); i++) {
+		for (int i = 0; i < numContact; i++) {
 			
-			float currentVel = contactArray[i]->calculateSeperatingVelocity();
+			float currentVel = (contactArray + i)->calculateSeperatingVelocity();
 			if (currentVel < smallestRelativeVelocity) {
 				smallestRelativeVelocity = currentVel;
-				contactToResolve = contactArray[i];
+				contactToResolve = (contactArray + i);
 			}
 		}
 		if (contactToResolve != nullptr)
 			contactToResolve->resolve(duration);
 		else
 			break;
-	} while (m_iteration < numContact * 2);*/
+	} while (m_iteration < numContact * 2);
 	
-	
-	for (int i = 0; i < numContact; i++) {
-		(contactArray+i)->resolve(duration);
-	}
 }
