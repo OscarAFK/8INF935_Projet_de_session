05be3c418c710b085409391ab5d1f25947b8da51
Osar <o.trosseau@laposte.net>
2021-12-06 10:07:04 -0500

    Renamed files

--- MPJV - Projet/MPJV - Projet/Colliders/ContactGeneratorBroadphase.h
+++ MPJV - Projet/MPJV - Projet/Colliders/ContactGeneratorBroadphase.h
@@ -2,13 +2,13 @@
 #include <vector>
 #include "Contact.h"
 
-class ContactGenerator
+class ContactGeneratorBroadphase
 {
 private:
 	std::vector<Rigidbody>* m_rigidbodies;
 
 public:
-	ContactGenerator(std::vector<Rigidbody>* rigidbodies) :m_rigidbodies(rigidbodies) {};
+	ContactGeneratorBroadphase(std::vector<Rigidbody>* rigidbodies) :m_rigidbodies(rigidbodies) {};
 
 	virtual unsigned int addContact(Contact* contact, unsigned int  limit) = 0;
 };
